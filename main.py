2af1d7cbb3ce3006a122e53147bf2e108aeedcb4
Jakub-Kwak <jakub.kwak@toptal.com>
2021-08-27 09:22:16 +0200

    archive csv

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from github import Github
 import sys
+import csv
 
 print("Hello Repo")
 token = sys.argv[1]
@@ -8,3 +9,9 @@ repos = g.get_user("jakub-qak").get_repos()
 repos_list = [repo.name for repo in repos]
 print(repos_list)
 print(len(repos_list))
+
+with open("repos.csv", "w", newline="") as csv_file:
+    writer = csv.writer(csv_file)
+    writer.writerow(["Repo"])
+    for repo in repos_list:
+        writer.writerow([repo])
